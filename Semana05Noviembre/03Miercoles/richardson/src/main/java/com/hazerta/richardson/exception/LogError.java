03cd16f071192c9836503bf88ba00bf4c0f84e49
Alberto <albertodepablolopez@gmail.com>
2025-11-19 14:30:41 +0100

    19nov 2

--- Semana05Noviembre/03Miercoles/richardson/src/main/java/com/hazerta/richardson/exception/LogError.java
+++ Semana05Noviembre/03Miercoles/richardson/src/main/java/com/hazerta/richardson/exception/LogError.java
@@ -1,4 +1,17 @@
 package com.hazerta.richardson.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class LogError {
+
+    private LocalDate fecha;
+    private String Detalle;
 }
